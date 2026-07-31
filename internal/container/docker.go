d1c44f10e1c31d4c8a6c877f407ae9f2dfdb9c51
Braedyn L <poohdie10@gmail.com>
2026-07-30 23:56:10 -0400

    Add Docker client initialization and update proto memory_limit type to int64

--- internal/container/docker.go
+++ internal/container/docker.go
@@ -1,1 +1,18 @@
 package container
+
+import (
+	"log"
+
+	"github.com/moby/moby/client"
+)
+
+func container() { // not permantly func(for now atleast im just experimenting)
+	cli, err := client.New(client.FromEnv)
+	if err != nil {
+		log.Fatal(err)
+	}
+	if cli == nil { //temp because i CANT STAND error underlines
+
+	}
+
+}
