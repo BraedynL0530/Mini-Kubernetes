48706fb13b2bd1300bc5f6de02d8498158368206
Braedyn L <poohdie10@gmail.com>
2026-08-02 00:14:21 -0400

    Refactor Docker package, add Engine interface with mock implementation, and fix Cat struct formatting

---
+++ internal/dockerx/docker.go
@@ -0,0 +1,60 @@
+package dockerx
+
+//renamed because go compling
+
+import (
+	"context"
+
+	"github.com/moby/moby/api/types/container"
+)
+
+type Engine interface {
+	Pull(ctx context.Context, image string) (string, error)
+	Create(ctx context.Context, image string, hostConfig container.HostConfig) (string, error)
+	Start(ctx context.Context, container string) (string, error)
+	Stop(ctx context.Context, container string) (string, error)    // may need to add a timeout for chill shutdown
+	Restart(ctx context.Context, container string) (string, error) // maybe on sick containers? or after a good stop? idk yet just figured id add it(40s prehaps)
+	Kill(ctx context.Context, container string) (string, error)    // after no heartbeat for 300 seconds(5 minutes kill that boy and delte em)
+}
+
+type fakeDocker struct{}
+
+func (f fakeDocker) Pull(ctx context.Context, image string) (string, error) {
+	return "nginx:alpine", nil
+}
+
+func (f fakeDocker) Create(ctx context.Context, image string, hostConfig container.HostConfig) (string, error) {
+	return "paw-123 Created!", nil
+}
+
+func (f fakeDocker) Start(ctx context.Context, image string) (string, error) {
+	return "starting paw-123...", nil
+}
+
+func (f fakeDocker) Stop(ctx context.Context, image string) (string, error) {
+	return "stopping paw-123...", nil
+}
+
+func (f fakeDocker) Restart(ctx context.Context, image string) (string, error) {
+	return "restarting paw-123...", nil
+}
+
+func (f fakeDocker) Kill(ctx context.Context, image string) (string, error) {
+	return "killing paw-123... x_x", nil
+}
+
+//var cli *client.Client
+
+func initDocker() {
+
+}
+
+func startContainer() {
+
+}
+
+func stopContainer() {}
+
+func removeContainer() {}
+
+//ToDo: grow a brain
--- pkg/types/cats.go
+++ pkg/types/cats.go
@@ -3,15 +3,15 @@ package types
 import "time"
 
 type Cat struct {
-	Name string `json:"name"`
+	Name      string `json:"name"`
 	IpAddress string `json:"ip_address"`
-	Port int `json:"port"` // gRpc port
+	Port      int    `json:"port"` // gRpc port
 
 	TotalCpu float64 `json:"total_cpu"`
-    TotalRam int64   `json:"total_ram"`
+	TotalRam int64   `json:"total_ram"`
 	RamUsage float64 `json:ram_usage`
-	CpuUsage float64 `json:Cpu_Usage`
+	CpuUsage float64 `json:cpu_Usage`
 
-	Status string `json:"status"` //Active, Asleep, Dead
+	Status        string    `json:"status"` //Active, Asleep, Dead
 	LastHeartbeat time.Time `json:"last_heartbeat"`
 }
