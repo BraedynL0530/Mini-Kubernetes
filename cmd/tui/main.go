61fa968c3f8e4124a7e87ee8ca3bb4a366aac6b2
Braedyn L <poohdie10@gmail.com>
2026-08-03 14:57:26 -0400

    Refactor TUI and configuration parsing: fix formatting, update comments, and implement YAML parsing

--- cmd/tui/main.go
+++ cmd/tui/main.go
@@ -1,34 +1,36 @@
 package main
 
 import (
-    "fmt"
-    "os"
+	"fmt"
 
-    tea "charm.land/bubbletea/v2"
+	tea "charm.land/bubbletea/v2"
 )
 
-//ToDO: add screens, finish sub menus and options, this is skeleton untill i have actual functions 
+//ToDO: add screens, finish sub menus and options, this is skeleton untill i have actual functions
 
 type Screen int
 
 const (
-    mainScreen Screen = iota
-    GetSubMenu
+	mainScreen Screen = iota
+	GetSubMenu
 	ApplyResultScreen
 )
+
 // Bubble Tea Docs i stole and edited
 type model struct {
-    choices  []string           // items on the to-do list
-    cursor   int                // which to-do list item our cursor is pointing at
-    selected map[int]struct{}   // which to-do items are selected
-	getSubChoices []string      // items on the get sub-menu
-	currentScreen Screen  		// current screen being displayed
-	outputMessage string		// message to display on the apply result screen
+	choices       []string         // items on the to-do list
+	cursor        int              // which to-do list item our cursor is pointing at
+	selected      map[int]struct{} // which to-do items are selected
+	getSubChoices []string         // items on the get sub-menu
+	currentScreen Screen           // current screen being displayed
+	outputMessage string           // message to display on the apply result screen
 }
+
+// add a method for like -r path/to/ymal file
 
 func initialModel() model {
 	return model{
-		choices:  []string{"apply", "get", "delete"},
+		choices:       []string{"apply", "get", "delete"},
 		getSubChoices: []string{"fetch logs", "fetch metrics", "back to main"},
 
 		// A map which indicates which choices are selected. We're using
@@ -37,76 +39,76 @@ func initialModel() model {
 }
 
 func (m model) Init() tea.Cmd {
-    return nil
+	return nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type) {
+	switch msg := msg.(type) {
 
-    // Is it a key press?
-    case tea.KeyPressMsg:
+	// Is it a key press?
+	case tea.KeyPressMsg:
 
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
 
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            return m, tea.Quit
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
 
-        // The "up" and "k" keys move the cursor up
-        case "up", "k":
-            if m.cursor > 0 {
-                m.cursor--
-            }
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
 
-        // The "down" and "j" keys move the cursor down
-        case "down", "j":
-            if m.cursor < len(m.choices)-1 {
-                m.cursor++
-            }
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
 
-        // The "enter" key and the space bar toggle the selected state
-        // for the item that the cursor is pointing at.
-        case "enter", "space":
-            _, ok := m.selected[m.cursor]
-            if ok {
-                delete(m.selected, m.cursor)
-            } else {
-                m.selected[m.cursor] = struct{}{}
-            }
-        }
-    }
+		// The "enter" key and the space bar toggle the selected state
+		// for the item that the cursor is pointing at.
+		case "enter", "space":
+			_, ok := m.selected[m.cursor]
+			if ok {
+				delete(m.selected, m.cursor)
+			} else {
+				m.selected[m.cursor] = struct{}{}
+			}
+		}
+	}
 
-    // Return the updated model to the Bubble Tea runtime for processing.
-    // Note that we're not returning a command.
-    return m, nil
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
 }
 
 func (m model) View() tea.View {
-    // The header
-    s := "Meowbernetes  \n\n What would you like to do?\n\n"
+	// The header
+	s := "Meowbernetes  \n\n What would you like to do?\n\n"
 
-    // Iterate over our choices
-    for i, choice := range m.choices {
+	// Iterate over our choices
+	for i, choice := range m.choices {
 
-        // Is the cursor pointing at this choice?
-        cursor := " " // no cursor
-        if m.cursor == i {
-            cursor = ">" // cursor!
-        }
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
 
-        // Is this choice selected?
-        checked := " " // not selected
-        if _, ok := m.selected[i]; ok {
-            checked = "x" // selected!
-        }
+		// Is this choice selected?
+		checked := " " // not selected
+		if _, ok := m.selected[i]; ok {
+			checked = "x" // selected!
+		}
 
-        // Render the row
-        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
-    }
+		// Render the row
+		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+	}
 
-    // The footer
-    s += "\nPress q to quit.\n"
+	// The footer
+	s += "\nPress q to quit.\n"
 
-    return tea.NewView(s)
+	return tea.NewView(s)
 }
--- internal/config/parser.go
+++ internal/config/parser.go
@@ -1,5 +1,12 @@
 package config
 
+import (
+	"fmt"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
 type Container struct {
 	Name  string `yaml:"name"`
 	Image string `yaml:"image"`
@@ -8,7 +15,7 @@ type Container struct {
 type PawConfig struct {
 	Name         string      `yaml:"name"`
 	Containers   []Container `yaml:"containers"`
-	NodeName     string      `yaml:"nodeName"`     // checked docs labels are how u organize, label matches with cat label and becomes like nodes pod/child.
+	NodeName     string      `yaml:"nodeName"`     // checked docs labels are how you organize, the label matches with cat label and becomes like node pod/child.
 	DesiredState int64       `yaml:"desiredState"` // how many pods
 	CatSurge     int64       `yaml:"catSurge"`     // max over desired state temp
 }
@@ -24,4 +31,16 @@ type ClusterConfig struct {
 	CatsConfigs []CatConfig `yaml:"cat"`
 }
 
-func Parse() {}
+func Parse(filePath string) (*ClusterConfig, error) {
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read file %s: %w", filePath, err)
+	}
+
+	var config ClusterConfig
+	if err := yaml.Unmarshal(data, &config); err != nil {
+		return nil, fmt.Errorf("failed parse ymal content %w", filePath, err)
+	}
+
+	return &config, nil
+}
